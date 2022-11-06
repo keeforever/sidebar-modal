fff4e2c57cc88657f7b62276f37a660a509a4ecb
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-06 09:13:24 +0000

    chore: maintenance

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,30 @@
-import {FaRegTimesCircle} from 'react-icons/fa'
-import { useGlobalContext } from '../context'
+import { FaRegTimesCircle } from "react-icons/fa";
+import { useGlobalContext } from "../context";
 
 const Modal = () => {
-  const {isModalOpen,modalHandler}=useGlobalContext()
-  console.log(useGlobalContext())
+  const { isModalOpen, modalHandler } = useGlobalContext();
   return (
     <>
-    <div className={isModalOpen ? 'modal-overlay modal-overlay-show' : 'modal-overlay modal-overlay-hide'}>
-      <div className="modal__content-container">
-        <div className="modal__close__btn-wrapper">
-          <button className='btn' onClick={modalHandler}><FaRegTimesCircle/></button>
-        </div>
-        <div className="modal__text-wrapper">
-          <h1>{'Show Modal'}</h1>
+      <div
+        className={
+          isModalOpen
+            ? "modal-overlay modal-overlay-show"
+            : "modal-overlay modal-overlay-hide"
+        }
+      >
+        <div className="modal__content-container">
+          <div className="modal__close__btn-wrapper">
+            <button className="btn" onClick={modalHandler}>
+              <FaRegTimesCircle />
+            </button>
+          </div>
+          <div className="modal__text-wrapper">
+            <h1>{"Show Modal"}</h1>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
